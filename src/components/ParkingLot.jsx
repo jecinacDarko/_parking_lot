d251617c7f679549db2d845ec6665337b9082a4f
jecinacDarko <darkojecinac@gmail.com>
2023-04-10 22:29:15 +0200

    working on layout

--- src/components/ParkingLot.jsx
+++ src/components/ParkingLot.jsx
@@ -1,9 +1,41 @@
 import React from 'react';
+import ParkingPlace from './ParkingPlace';
+import './ParkingLot.css';
 
 const ParkingLot = () => {
-  return (
-    <div>ParkingLot</div>
-  )
-}
+  const isValidPlace = (row, col) => {
+    return !(
+      row === 2 ||
+      row === 5 ||
+      (row === 3 && (col <= 2 || col >= 14)) ||
+      (row === 4 && (col <= 2 || col >= 14))
+    );
+  };
+
+  const gridItems = Array.from({ length: 6 * 16 }, (_, index) => {
+    const row = Math.floor(index / 16) + 1;
+    const col = (index % 16) + 1;
+
+    if (isValidPlace(row, col)) {
+      const placeNumber = (row - 1) * 16 + col - (row > 2 && row < 5 ? 2 : 0);
+      return (
+        <div
+          className="parking-place-wrapper"
+          key={`${row}-${col}`}
+          style={{
+            gridColumn: col,
+            gridRow: row,
+          }}
+        >
+          <ParkingPlace number={placeNumber} />
+        </div>
+      );
+    }
+
+    return null;
+  }).filter(Boolean);
+
+  return <div className="parking-lot">{gridItems}</div>;
+};
 
 export default ParkingLot;
--- src/components/ParkingPlace.jsx
+++ src/components/ParkingPlace.jsx
@@ -1,9 +1,30 @@
-import React from 'react';
+// ParkingPlace.js
 
-const ParkingPlace = () => {
+import React, { useState, useCallback } from 'react';
+import './ParkingPlace.css';
+
+const ParkingPlace = ({ number }) => {
+  const [isOccupied, setIsOccupied] = useState(false);
+  const [bounce, setBounce] = useState(false);
+
+  const handleClick = useCallback(() => {
+    setIsOccupied(!isOccupied);
+    setBounce(true);
+    console.log(` ${isOccupied ? 'Goodbye!' : 'Welcome!'}`);
+
+    setTimeout(() => {
+      setBounce(false);
+    }, 300);
+  }, [isOccupied, number]);
+
   return (
-    <div>ParkingPlace</div>
-  )
-}
+    <div
+      className={`parking-place${isOccupied ? ' occupied' : ''}${bounce ? ' bounce' : ''}`}
+      onClick={handleClick}
+    >
+      {number}
+    </div>
+  );
+};
 
 export default ParkingPlace;
